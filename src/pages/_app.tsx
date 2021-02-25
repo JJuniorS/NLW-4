c4f4af1477eb672bba075f74ab0275f8e77fbfc4
Joao S# <myannotation.dev@gmail.com>
2021-02-24 22:24:21 -0300

    Codigo Aula #3
    #focopraticagrupo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/global.css'
 
+import { ChallengesProvider, ChallengesContext } from '../contexts/ChallengesContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 
+import { ChallengeBox } from "../components/ChallengeBox";
+
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
@@ -22,7 +24,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>

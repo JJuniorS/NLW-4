322bd03a5e7637a1686c8d8a2784629eac343ccc
Joao S# <myannotation.dev@gmail.com>
2021-02-25 20:06:10 -0300

    Codigo Aula #4
    #NeverStopLearning

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,21 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox () {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
+    const { resetCountdown } = useContext(CountdownContext);
 
+    function handleChallengeSucceeded() {
+        completeChallenge();
+        resetCountdown();
+    }
+
+    function handleChallengeFail() {
+        resetChallenge();
+        resetCountdown();
+    }
 
     return (
         <div className={styles.challengeBoxContainer}>
@@ -22,7 +33,7 @@ export function ChallengeBox () {
                         <button 
                             type="button"
                             className={styles.challengeFailedButton}
-                            onClick={resetChallenge}
+                            onClick={handleChallengeFail}
                         >
                             Falhei
                         </button>
@@ -30,6 +41,7 @@ export function ChallengeBox () {
                         <button 
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={handleChallengeSucceeded}
                         >
                             Completei
                         </button>
